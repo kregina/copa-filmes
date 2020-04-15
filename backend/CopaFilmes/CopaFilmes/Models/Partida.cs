4593f01a66bcc7c8ee067149692e286cb97b4648
Kelly Silva <kelly@kellyregina.com.br>
2020-04-15 15:54:09 -0300

    add partida model

---
+++ backend/CopaFilmes/CopaFilmes/Models/Partida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopaFilmes.Models
+{
+    public class Partida
+    {
+        public IEnumerable<Filme> Resultado { get; private set; }
+        public Filme Vencedor => Resultado.First();
+        public Partida(params Filme[] filmes)
+        {
+            Resultado = filmes
+                .OrderBy(filme => filme.Nota)
+                .ThenBy(filme => filme.Titulo);
+        }
+    }
+}
