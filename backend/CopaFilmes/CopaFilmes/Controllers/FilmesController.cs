ee511ad414cf56f4bf2c45fe63bb969b1d4471ab
Kelly Silva <kelly@kellyregina.com.br>
2020-04-15 15:45:15 -0300

    add filmes controller

---
+++ backend/CopaFilmes/CopaFilmes/Controllers/FilmesController.cs
@@ -0,0 +1,27 @@
+﻿using CopaFilmes.Models;
+using CopaFilmes.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CopaFilmes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        private readonly CopaFilmesApiService _service;
+
+        public FilmesController(CopaFilmesApiService service) 
+        {
+            _service = service;
+        }
+
+        // GET: api/Filmes
+        [HttpGet]
+        public async Task<IEnumerable<Filme>> GetAsync()
+        {
+            return await _service.GetFilmes();
+        }
+    }
+}
