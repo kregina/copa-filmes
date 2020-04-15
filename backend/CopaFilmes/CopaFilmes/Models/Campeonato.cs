ba0de9ceb75776a133d366047002c3aa7b52a705
Kelly Silva <kelly@kellyregina.com.br>
2020-04-15 16:17:06 -0300

    add elimimnatoria e campeonato model

---
+++ backend/CopaFilmes/CopaFilmes/Models/Campeonato.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CopaFilmes.Models
+{
+    public class Campeonato
+    {
+        public IEnumerable<Filme> Competidores { get; private set; }
+
+        public Campeonato(IEnumerable<Filme> filmes)
+        {
+            Competidores = filmes.OrderBy(filme => filme.Titulo);
+        }
+
+        public Partida Final() => Eliminatorias().Last().Partidas.First();
+
+        public IEnumerable<Filme> ResultadoFinal() => Final().Resultado;
+        public IEnumerable<Eliminatoria> Eliminatorias()
+        {
+            var eliminatoria = Eliminatoria.CriarPrimeiraEliminatoria(Competidores);
+            yield return eliminatoria;
+
+            while(eliminatoria.Partidas.Count() > 1)
+            {
+                eliminatoria = Eliminatoria.CriarEliminatoriaSubsequente(eliminatoria.Vencedores);
+                yield return eliminatoria;
+            }
+        }
+    }
+}
---
+++ backend/CopaFilmes/CopaFilmes/Models/Eliminatoria.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CopaFilmes.Models
+{
+    public class Eliminatoria
+    {
+        public IEnumerable<Partida> Partidas { get; private set; }
+        public IEnumerable<Filme> Vencedores => Partidas.Select(partida => partida.Vencedor);
+
+        private Eliminatoria(IEnumerable<Partida> partidas)
+        {
+            Partidas = partidas;
+        }
+
+        public static Eliminatoria CriarPrimeiraEliminatoria(IEnumerable<Filme> competidores)
+        {
+            var partidas = competidores.PrimeiroComUltimo()
+                .Select(combinacao => new Partida(combinacao.Primeiro, combinacao.Ultimo));
+
+            return new Eliminatoria(partidas);
+        }
+
+        public static Eliminatoria CriarEliminatoriaSubsequente(IEnumerable<Filme> competidores)
+        {
+            var partidas = competidores.PrimeiroComSegundo()
+                .Select(combinacao => new Partida(combinacao.Primeiro, combinacao.Segundo));
+
+            return new Eliminatoria(partidas);
+        }
+
+    }
+}
