c4f2c76f8b1cfde7e57aa1c38b0f097f38603b05
Kelly Silva <kelly@kellyregina.com.br>
2020-04-15 16:22:08 -0300

    add campeonato controller e criar campeonato model

---
+++ backend/CopaFilmes/CopaFilmes/Controllers/CampeonatosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CopaFilmes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopaFilmes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampeonatosController : ControllerBase
+    {
+        // POST: api/Campeonatos
+        [HttpPost]
+        public IEnumerable<Filme> Post(CriarCampeonato comando)
+        {
+            var campeonato = new Campeonato(comando.Filmes);
+            return campeonato.ResultadoFinal();
+        }
+    }
+}
---
+++ backend/CopaFilmes/CopaFilmes/Models/CriarCampeonato.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CopaFilmes.Models
+{
+    public class CriarCampeonato
+    {
+        [MinLength(8)]
+        [MaxLength(8)]
+        public IEnumerable<Filme> Filmes { get; set; }
+    }
+}
